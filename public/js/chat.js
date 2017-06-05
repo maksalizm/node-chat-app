7b9d50f4d493e19abdac1f016b17cfc7fb031572
maksalizm <maksalizm@gmail.com>
2017-06-05 20:12:52 +0900

    Add Users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -29,8 +29,12 @@ socket.on('connect', function () {
     });
 });
 
-socket.on('updateUserList', function () {
-
+socket.on('updateUserList', function (users) {
+    var ol = $('<ol></ol>');
+    users.forEach(function (user) {
+        ol.append($('<li></li>').text(user));
+    });
+    $('#users').html(ol);
 });
 
 socket.on('disconnect', function () {
